11a64382f1eb64b778f91111de605b2aac94e10e
ascotlan <antonio.k.scotland@gmail.com>
2023-05-28 12:09:00 +0000

    Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.

---
+++ findKey.js
@@ -0,0 +1,42 @@
+// Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+
+const findKey = function(obj, callback) {
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+const results1 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+);
+
+const results2 = findKey(
+  {
+    ajaxsettings: { ak1: "v1", ak2: "v2" },
+    uisettings: { ui1: "v1", ui22: "v2" },
+  },
+  (x) => x.ui22 === "v2"
+);
+
+assertEqual(results1, "noma");
+assertEqual(results2, "uisettings");
